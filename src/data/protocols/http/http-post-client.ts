df7818c85508f96d50ac56986edea3b91b1fb6ab
vitorcontiz <vitorcontizgit@gmail.com>
2022-04-28 22:38:34 -0300

    refactor: group imports in index files

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,10 +1,10 @@
-import { HttpResponse } from './http-response'
+import { HttpResponse } from '.';
 
 export type HttpPostParams<T> = {
-  url: string
-  body: T
-}
+    url: string;
+    body: T;
+};
 
 export interface HttpPostClient<T, R> {
-  post(params: HttpPostParams<T>): Promise<HttpResponse<R>>
+    post(params: HttpPostParams<T>): Promise<HttpResponse<R>>;
 }
--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,11 +1,9 @@
 import {
     HttpPostClient,
-    HttpPostParams
-} from '../protocols/http/http-post-client';
-import {
+    HttpPostParams,
     HttpResponse,
     HttpStatusCode
-} from '@/data/protocols/http/http-response';
+} from '../protocols/http';
 
 export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
     url?: string;
