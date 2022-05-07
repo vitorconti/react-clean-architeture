39cf001d5ddde16b2a4a2da0d6500aa2c8c0ea50
vitorcontiz <vitorcontizgit@gmail.com>
2022-05-07 15:42:37 -0300

    refactor: move input to a separate file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,8 +1,9 @@
-import Footer from '@/presentation/components/footer/footer';
-import Header from '@/presentation/components/login/login-header';
-import Spinner from '@/presentation/components/spinner/spinner';
-import React from 'react';
-import Styles from './login-styles.scss';
+import Footer from '@/presentation/components/footer/footer'
+import Input from '@/presentation/components/input/input'
+import Header from '@/presentation/components/login/login-header'
+import Spinner from '@/presentation/components/spinner/spinner'
+import React from 'react'
+import Styles from './login-styles.scss'
 
 const Login: React.FC = () => {
     return (
@@ -10,24 +11,18 @@ const Login: React.FC = () => {
             <Header />
             <form className={Styles.form}>
                 <h2>Login</h2>
-                <div className={Styles.inputWrap}>
-                    <input
-                        type="email"
-                        name="email"
-                        id=""
-                        placeholder="Digite seu e-mail"
-                    />
-                    <span className={Styles.status}>🔴</span>
-                </div>
-                <div className={Styles.inputWrap}>
-                    <input
-                        type="password"
-                        name=""
-                        id=""
-                        placeholder="Digite sua senha"
-                    />
-                    <span className={Styles.status}>🔴</span>
-                </div>
+                <Input
+                    type="email"
+                    name="email"
+                    id=""
+                    placeholder="Digite seu e-mail"
+                />
+                <Input
+                    type="password"
+                    name="password"
+                    id=""
+                    placeholder="Digite sua senha"
+                />
 
                 <button className={Styles.submit} type="submit">
                     Entrar
@@ -40,6 +35,6 @@ const Login: React.FC = () => {
             </form>
             <Footer />
         </div>
-    );
-};
-export default Login;
+    )
+}
+export default Login
