d1f4aae28924d509a98b8b0af892c0d12437767b
vitorcontiz <vitorcontizgit@gmail.com>
2022-05-09 20:36:25 -0300

    feat: ensure Login won't load spinner and error on start

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -4,34 +4,46 @@ import {
     LoginHeader as Header,
     FormStatus
 } from '@/presentation/components'
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './login-styles.scss'
+import Context from '@/presentation/contexts/form/form-context'
+type StateProps = {
+    isLoading: boolean
+    errorMessage: string
+}
 
 const Login: React.FC = () => {
+    const [state] = useState<StateProps>({
+        isLoading: false,
+        errorMessage: ''
+    })
     return (
         <div className={Styles.login}>
             <Header />
-            <form className={Styles.form}>
-                <h2>Login</h2>
-                <Input
-                    type="email"
-                    name="email"
-                    id=""
-                    placeholder="Digite seu e-mail"
-                />
-                <Input
-                    type="password"
-                    name="password"
-                    id=""
-                    placeholder="Digite sua senha"
-                />
+            <Context.Provider value={state}>
+                <form className={Styles.form}>
+                    <h2>Login</h2>
+                    <Input
+                        type="email"
+                        name="email"
+                        id=""
+                        placeholder="Digite seu e-mail"
+                    />
+                    <Input
+                        type="password"
+                        name="password"
+                        id=""
+                        placeholder="Digite sua senha"
+                    />
 
-                <button className={Styles.submit} type="submit">
-                    Entrar
-                </button>
-                <span className={Styles.link}>Criar conta</span>
-                <FormStatus />
-            </form>
+                    <button className={Styles.submit} type="submit">
+                        Entrar
+                    </button>
+                    <span className={Styles.link}>Criar conta</span>
+                    <FormStatus />
+                </form>
+            </Context.Provider>
+
             <Footer />
         </div>
     )
