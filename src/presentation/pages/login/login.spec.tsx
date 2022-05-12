b52ccf8d7377d51cf6a974f08715830a98f80c90
vitorcontiz <vitorcontizgit@gmail.com>
2022-05-11 21:45:58 -0300

    test: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -46,7 +46,7 @@ describe('Login Component', () => {
         expect(passwordStatus.title).toBe('Campo obrigatório')
         expect(passwordStatus.textContent).toBe('🔴')
     })
-    test('Should call Validation with correct values', () => {
+    test('Should call Validation with correct email', () => {
         const { sut, validationSpy } = makeSut()
         const emailInput = sut.getByTestId('email')
         fireEvent.input(emailInput, {
@@ -56,7 +56,7 @@ describe('Login Component', () => {
         })
     })
 
-    test('Should call Validation with correct values', () => {
+    test('Should call Validation with correct password', () => {
         const { sut, validationSpy } = makeSut()
         const passwordInput = sut.getByTestId('password')
         fireEvent.input(passwordInput, {
