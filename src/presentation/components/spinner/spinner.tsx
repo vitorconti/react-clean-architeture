a58dd8bb568152bf6b1f3e5a9992facfa5987c35
vitorcontiz <vitorcontizgit@gmail.com>
2022-05-15 22:31:35 -0300

    feat: ensure Login present spinner on submit

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import Styles from './spinner-styles.scss';
+import React from 'react'
+import Styles from './spinner-styles.scss'
 
-type Props = React.HTMLAttributes<HTMLElement>;
+type Props = React.HTMLAttributes<HTMLElement>
 const Spinner: React.FC<Props> = (props: Props) => {
     return (
-        <div {...props} className={[Styles.spinner, props.className].join(' ')}>
+        <div
+            {...props}
+            data-testid="spinner"
+            className={[Styles.spinner, props.className].join(' ')}
+        >
             <div />
             <div />
             <div />
             <div />
         </div>
-    );
-};
+    )
+}
 
-export default Spinner;
+export default Spinner
