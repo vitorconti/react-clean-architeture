d0eb23f8159ec632775271ab0b170cc042fe28e8
vitorcontiz <vitorcontizgit@gmail.com>
2022-05-30 20:56:19 -0300

    feat: prevent Login to submit form if its invalid

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -6,11 +6,15 @@ import Context from '@/presentation/contexts/form/form-context'
 const FormStatus: React.FC = () => {
     const { state } = useContext(Context)
     const { isLoading, mainError } = state
-
+    console.warn(state)
     return (
         <div data-testid="error-wrap" className={Styles.errorWrap}>
             {isLoading && <Spinner className={Styles.spinner} />}
-            {mainError && <span className={Styles.error}>{mainError}</span>}
+            {mainError && (
+                <span data-testid="main-error" className={Styles.error}>
+                    {mainError}
+                </span>
+            )}
         </div>
     )
 }
