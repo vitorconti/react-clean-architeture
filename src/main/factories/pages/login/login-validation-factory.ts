66c09c7bc78f26b253f28f41cbb3748c1a66d67e
vitorcontiz <vitorcontizgit@gmail.com>
2022-07-23 15:48:38 -0300

    refactor: move factories to their own files

---
+++ src/main/factories/pages/login/login-validation-factory.ts
@@ -0,0 +1,10 @@
+import { ValidationBuilder } from "@/validation/builder/validation-builder"
+import { ValidationComposite } from "@/validation/validators"
+
+export const makeLoginValidation = (): ValidationComposite => {
+    return ValidationComposite.build([
+        ...ValidationBuilder.field('email').required().email().build(),
+        ...ValidationBuilder.field('password').required().min(5).build(),
+
+    ])
+}
--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,7 +2,7 @@ import { Router } from '@/presentation/components'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import '@/presentation/styles/global.scss'
-import { makeLogin } from '@/presentation/components/router/factories/pages/login/login-factory'
+import { makeLogin } from '@/main/factories/pages/login/login-factory'
 ReactDOM.render(<Router
     makeLogin={makeLogin}
 />, document.getElementById('main'))
