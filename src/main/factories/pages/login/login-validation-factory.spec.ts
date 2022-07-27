58368226dde7204b704647cf05a35a2a9f0c541e
vitorcontiz <vitorcontizgit@gmail.com>
2022-07-26 21:29:36 -0300

    test: ensure ValidationComposite uses correct validations

---
+++ src/main/factories/pages/login/login-validation-factory.spec.ts
@@ -0,0 +1,13 @@
+import { ValidationBuilder } from "@/validation/builder/validation-builder"
+import { ValidationComposite } from "@/validation/validators"
+import { makeLoginValidation } from "./login-validation-factory"
+
+describe('LoginValidationFactory', () => {
+    test('Should make ValidationComposite with correct validations', () => {
+        const composite = makeLoginValidation()
+        expect(composite).toEqual(ValidationComposite.build([
+            ...ValidationBuilder.field('email').required().email().build(),
+            ...ValidationBuilder.field('password').required().min(5).build()
+        ]))
+    })
+})
