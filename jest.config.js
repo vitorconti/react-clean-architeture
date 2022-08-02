e42cd5e2da8e65ff57dd87d2d5aa5969d643d63e
vitorcontiz <vitorcontizgit@gmail.com>
2022-08-01 22:12:41 -0300

    chore: add files to coverage ignore

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
     roots: ['<rootDir>/src'],
-    collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.{ts,tsx}',
+        '!<rootDir>/src/main/**/*',
+        '!<rootDir>/src/presentation/components/router**/*',
+        '!<rootDir>/src/domain/models/index.ts',
+        '!<rootDir>/src/domain/usecases/index.ts',
+    '!**/*.d.ts'],
     coverageDirectory: 'coverage',
     testEnvironment: 'jsdom',
     transform: {
--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -8,7 +8,7 @@ import {
     RenderResult,
     waitFor
 } from '@testing-library/react'
-import Login from './login'
+import { Login } from '@/presentation/pages'
 import { ValidationStub, AuthenticationSpy } from '@/presentation/test/'
 import faker from 'faker'
 import { InvalidCredentialsError } from '@/domain/errors'
